dce3029389fba0131f2d40eb9b36662bcd11943e
github user name <tak7901y298@gmail.com>
2021-10-22 19:45:26 +0900

    initial commit

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,5 @@
+class FormsController < ApplicationController
+    def form_test
+        
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get "/form_test", to: "forms#form_test"
+end
