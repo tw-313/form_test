dd9087541c20abd11b535202d52037db46c80040
github user name <tak7901y298@gmail.com>
2021-10-22 20:20:50 +0900

    フォームと送信先の作成

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -2,4 +2,7 @@ class FormsController < ApplicationController
     def form_test
         
     end
+    def input_age
+        render plain:params["form_test"]["age"]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/form_test", to: "forms#form_test"
+  post "/create", to: "forms#input_age"
 end
